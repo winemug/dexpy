4e261c08ac16f7528d5b2ce2c5effbdbdf3780f9
Barış Kurtlutepe <barisk@gmail.com>
2018-10-07 23:12:21 +0200

    initial test version of receiver

--- dexcom_receiver/dexpy_receiver2console.py
+++ dexcom_receiver/dexpy_receiver2console.py
@@ -4,24 +4,21 @@ from dexpy_receiver import ReceiverSession
 import readdata
 
 def main():
-    parser = argparse.ArgumentParser()
 
-    # parser.add_argument("location", choices=[ "eu", "us" ])
-    # parser.add_argument("-u", "--username", required = True)
-    # parser.add_argument("-p", "--password", required = True)
-    # parser.add_argument("-i", "--sessionid", required = False)
+    parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", required = False, default = False, action='store_true')
 
     args = parser.parse_args()
 
     session = ReceiverSession()
-    gv = session.getLatestGlucoseValue()
-    print gv
+    session.startMonitoring()
     
     try:
         raw_input()
     except KeyboardInterrupt:
         pass
+
+    session.stopMonitoring()
 
 if __name__ == '__main__':
     main()
